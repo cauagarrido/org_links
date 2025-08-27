6a6098ea7011644384337a5110c73c6ecfba1be3
cauagarrido <cauagarrido@gmail.com>
2025-08-27 18:44:22 -0300

    criação de arquivos necessarios

---
+++ src/app/index/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+import {styles } from "./styles"
+
+export default function Index(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                
+            </View>
+        </View>
+    )
+}
+
+
