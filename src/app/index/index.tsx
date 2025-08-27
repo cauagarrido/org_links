041eab8f555616b5fc0e19847cfb319d688ef1a7
cauagarrido <cauagarrido@gmail.com>
2025-08-27 18:48:57 -0300

    logo

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -6,7 +6,7 @@ export default function Index(){
     return(
         <View style={styles.container}>
             <View style={styles.header}>
-                
+                <Image source={require("@/assets/logo.png")}/>
             </View>
         </View>
     )
